2cc5126c149916f0314311062cfcc26ec8c88b23
openOTK <58460323+openOTK@users.noreply.github.com>
2019-12-10 22:04:32 +0900

    別関数に統合
    不要になったので削除

--- xhr.js
+++
@@ -1,9 +0,0 @@
-var XHR = function(){
-return function(args){
-var httpObj = new XMLHttpRequest();
-httpObj.open(args["method"],args["url"],args["async"]);
-httpObj.responseType = args["type"];
-httpObj.onload = args["collback"](args);
-}
-httpObj.send();
-};
