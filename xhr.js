70fa3d3de3ab4ffd261261229b2bd14c1188c353
openOTK <58460323+openOTK@users.noreply.github.com>
2019-12-10 13:00:56 +0900

    ちょっと修正
    sendとか追加

--- xhr.js
+++ xhr.js
@@ -1,7 +1,9 @@
 var XHR = function(){
 return function(args){
 var httpObj = new XMLHttpRequest();
-httpObj.open(args["method"],args["url"],args["type"]);
+httpObj.open(args["method"],args["url"],args["async"]);
+httpObj.responseType = args["type"];
 httpObj.onload = args["collback"](args);
 }
+httpObj.send();
 };
