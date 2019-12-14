3a858d49868b366105f00a5a2868115c2b87e137
PoNIx2 <ponix2xone@gmail.com>
2019-12-15 03:27:20 +0900

    微バグ修正

--- js/FunctionContainer.js
+++ js/FunctionContainer.js
@@ -115,30 +115,6 @@ function FunctionContainer() {
                     }
                 });
             }
-        } else if(args === "deCodeInit") {
-            return function(args) {
-                // グラフ生成
-                var deCode_chart_elm = cp.D.querySelector(".deCode-chart");
-                var table_chart_elm = cp.D.createElement("table");
-                for(var i = 0, len_i = 8;i < len_i;i++) {
-                    var tr_chart_elm = cp.D.createElement("tr");
-                    for(var j = 0, len_j = 8;j < len_j;j++) {
-                        var td_chart_elm = cp.D.createElement("td");
-                        tr_chart_elm.appendChild(td_chart_elm);
-                    }
-                    table_chart_elm.appendChild(tr_chart_elm);
-                }
-                deCode_chart_elm.appendChild(table_chart_elm);
-                // パネル生成
-                var deCode_panel_elm = cp.D.querySelector(".deCode-panel");
-                var image_panel_elm = cp.D.createElement("img");
-                deCode_panel_elm.appendChild(image_panel_elm);
-                // コンテキスト生成
-                var deCode_context_elm = cp.D.querySelector(".deCode-context");
-                var input_context_elm = cp.D.createElement("input");
-                input_context_elm.setAttribute("type", "text");
-                deCode_context_elm.appendChild(input_context_elm);
-            }
         }
     }
 }
